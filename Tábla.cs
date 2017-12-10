8f75c53fadfc63cdb8c78476371249f1a1355024
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2017-12-10 06:46:27 +0100

    Up

--- Tábla.cs
+++ Tábla.cs
@@ -6,5 +6,18 @@ namespace OkjKiralynok
     {
         char[,] T; // 2. feladat
         char ÜresCella; // 2. feladat
+
+        public Tábla(char üresCella) // 3. feladat
+        {
+            T = new char[8, 8]; // 3.a
+            ÜresCella = üresCella; // 3.b
+            for (int i = 0; i < 8; i++) // 3.c
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    T[i, j] = ÜresCella; // 3.c
+                }
+            }
+        }
     }
 }
